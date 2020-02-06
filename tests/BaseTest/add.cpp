187fc7c11287311d2ebda774bd208361996f5cd0
Vladislav Semyonoff <vsemyonoff@gmail.com>
2020-02-07 01:12:35 +0200

    Fixed OBJECT library export for local subprojects

--- tests/BaseTest/add.cpp
+++ tests/BaseTest/add.cpp
@@ -1,7 +1,8 @@
 #define BOOST_TEST_MODULE BaseTest
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(TestAdd) {
+BOOST_AUTO_TEST_CASE(TestAdd)
+{
     int i = 2;
     BOOST_TEST(i);
     BOOST_TEST(i == 2);
